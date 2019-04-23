c9ae6e4d1aa40a1d899b14f1875d934510b1d3e4
Anca Teodora <ancasolot@gmail.com>
2019-04-23 17:47:00 +0300

    version 2.1

--- src/Tema_ISP/Orar.java
+++ src/Tema_ISP/Orar.java
@@ -1,14 +1,43 @@
 package Tema_ISP;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class Orar {
 
-	private CelulaOrar[] celule;
+	private ArrayList <CelulaOrar> celuleOrar = new ArrayList<CelulaOrar>();
 	
-	public void adaugareCelula(CelulaOrar celula) {}
+	public void adaugareCelula(CelulaOrar celula) {
+		this.celuleOrar.add(celula);
+		
+	}
 	
-	public void stergereCelula(CelulaOrar celula) {}
+	public void stergereCelula(CelulaOrar celula) {
+		if(this.celuleOrar.contains(celula))
+		{
+			celuleOrar.remove(celula);
+			
+		}
+		else
+		{
+			System.out.println("Aceasta celula nu exista!");
+		}
+		
+	}
 	
-	public void afisare() {}
+	public void afisare() {
+		for(CelulaOrar celulaOrar : celuleOrar)
+		{
+			celulaOrar.afisare();
+		}
+	}
 	
-	public void salvare(String fisier) {}
+	public void creare(CelulaOrar celula, String zi) {
+		Map<String, List<CelulaOrar>> orar = new HashMap<String, List<CelulaOrar>>();
+		orar.put(zi, celuleOrar);
+		
+	}
 }
