377d08b8acd13926de3b6ce263f08e0dd6557f57
Andreea26 <zarnescu.andreea23@yahoo.com>
2019-05-06 21:29:59 +0300

    GATAAAAA

--- src/Tema_ISP/Aplicatie.java
+++ src/Tema_ISP/Aplicatie.java
@@ -1,11 +1,11 @@
 package Tema_ISP;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 public class Aplicatie {
 
 	private static HashMap<String, String> conturi = new HashMap<String, String>();
-	
 
 	public static HashMap<String, String> getConturi() {
 		return conturi;
@@ -14,31 +14,30 @@ public class Aplicatie {
 	public void setConturi(HashMap<String, String> conturi) {
 		Aplicatie.conturi = conturi;
 	}
-	
-	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		//grupe
-		Grupa grupa1 = new Grupa("332","AB");
+		// grupe
+		Grupa grupa1 = new Grupa("332", "AB");
 		grupa1.adaugareMaterie("IRA");
 		grupa1.adaugareMaterie("Baze de date");
 		grupa1.adaugareMaterie("POO");
 		grupa1.adaugareMaterie("SDA");
 		grupa1.adaugareMaterie("Fizica");
 
-		//profesori
+		// profesori
 		Profesor prof1 = new Profesor("Caramihai Mihai");
 		Profesor prof2 = new Profesor("Caramihai Simona");
 		Profesor prof3 = new Profesor("Veizu Ileana");
-		
+
 		prof1.adaugareMaterie("SDA");
 		prof1.adaugareMaterie("POO");
 		prof2.adaugareMaterie("IRA");
 		prof2.adaugareMaterie("Fizica");
 		prof3.adaugareMaterie("Baze de date");
-		
-		//secretar
+	
+
+		// secretar
 		Secretar secretar1 = new Secretar();
 		secretar1.adaugareNumeMateriePredefinita("Matematica");
 		secretar1.adaugareNumeMateriePredefinita("Fizica");
@@ -47,10 +46,19 @@ public class Aplicatie {
 		secretar1.adaugareNumeMateriePredefinita("POO");
 		secretar1.adaugareNumeMateriePredefinita("SDA");
 		secretar1.adaugareNumeMateriePredefinita("IRA");
-		
+
+		secretar1.adaugareProfesori(prof1);
+		secretar1.adaugareProfesori(prof2);
+		secretar1.adaugareProfesori(prof3);
 		
 		secretar1.creareMaterie(grupa1, DesfasurareMaterie.curs);
+		secretar1.creareCelulaOrar();
+		secretar1.creareMaterie(grupa1, DesfasurareMaterie.laborator);
+		secretar1.creareCelulaOrar();
 		
+		secretar1.afisareOrar();
+		secretar1.salvareOrar("date.txt");
+
 	}
 
 }
